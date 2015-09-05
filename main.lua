2810a4d460d023469999a854865e0f6a6486f0ad
Jenna <jenna.maccarley@gmail.com>
2015-09-05 02:02:59 -0400

    finished game, added comments

--- main.lua
+++ main.lua
@@ -1,9 +1,15 @@
+-- "Flappy Bird" intro to programming tutorial program
+-- Intended to be a simple rendition of the popular game for use to teach
+-- intro engineering students a little bit about programming
+
+-- Uses the Lua programming language and the LOVE 2D game engine here:
+-- https://love2d.org/
+
+-- By Jenna MacCarley
+
 debug = true
 
 -- Collision detection taken function from http://love2d.org/wiki/BoundingBox.lua
--- Returns true if two boxes overlap, false if they don't
--- x1,y1 are the left-top coords of the first box, while w1,h1 are its width and height
--- x2,y2,w2 & h2 are the same, but for the second box
 function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
   return x1 < x2+w2 and
          x2 < x1+w1 and
@@ -11,30 +17,32 @@ function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
          y2 < y1+h1
 end
 
-function drawPillar()
-	love.graphics.rectangle('fill', pillar.x, 0, 50, pillar.y)
-	love.graphics.rectangle('fill', pillar.x, pillar.y + 200, 50, 800)
-end
-
-
+-- Important game state variables including position of bird, pillars, score, and game status
 lostGame = false
 flappyBird = {x = 250, y = 300, speed = 150, img = nil}
 pillar = {x = 600, y = 400}
 score = 0
 
+-- Draw the collision pillars
+function drawPillar()
+	love.graphics.rectangle('fill', pillar.x, 0, 50, pillar.y)
+	love.graphics.rectangle('fill', pillar.x, pillar.y + 200, 50, 800)
+end
+
+-- This is called at the very beginning of the love engine when it launches
 function love.load(arg)
 	flappyBird.img = love.graphics.newImage('assets/flappy_bird.png')
 	love.graphics.setBackgroundColor(0, 0, 200)
-
 end
 
-
+-- This is called after each frame update in the game. Updates current game state
 function love.update(dt)
-	-- Exit the game
+	-- Exit the game if escape key is pressed
 	if love.keyboard.isDown('escape') then
 		love.event.push('quit')
 	end
-
+    
+    -- At the end of the game, the spacebar may be pressed to restart the game
 	if love.keyboard.isDown(' ') and lostGame then
 		love.graphics.setBackgroundColor(0, 0, 200)
 		lostGame = false
@@ -44,23 +52,26 @@ function love.update(dt)
 		pillar = {x = 600, y = 400}
 		score = 0
 	end
-
-	if love.keyboard.isDown('up', 'w') then
+    
+    -- During the game, the up key is used to control the bird
+	if love.keyboard.isDown('up', 'w') and not lostGame then
 		flappyBird.speed = flappyBird.speed + 50 + score
 	end
-
+    
+    -- If the bird exits the frame, the game ends
 	if flappyBird.y > 750 or flappyBird.y < 0 then
 		lostGame = true
 	end
 
+    -- If the bird collides with a pillar, the game also ends
 	if CheckCollision(pillar.x, 0, 50, pillar.y, flappyBird.x, flappyBird.y, flappyBird.img:getWidth()*.2, flappyBird.img:getHeight()*.2) then
 		lostGame = true
 	end
-
 	if CheckCollision(pillar.x, pillar.y + 200, 50, 800, flappyBird.x, flappyBird.y, flappyBird.img:getWidth()*.2, flappyBird.img:getHeight()*.2) then
 		lostGame = true
 	end
 
+    -- If the game is still going, update the pillar position and bird position
     if not lostGame then
 		pillar.x = pillar.x - 5;
 	    flappyBird.y = flappyBird.y - (flappyBird.speed*dt)
@@ -68,25 +79,29 @@ function love.update(dt)
 	   flappyBird.speed = flappyBird.speed - 20 - score
 	end 
 
+    -- Reset the pillar if it has passed across the screen
 	if pillar.x <= 0 then
 		pillar.x = 600
 		pillar.y = 200 + love.math.random(400)
 	end
 
+    -- When the bird crosses the pillar, the score increases
 	if pillar.x == 250 then
 		score = score + 1
 	end
 
 end
 
-
+-- This function is called after each frame update in the game. Updates the game canvas
 function love.draw(dt)
+	-- If the game hasnt ended, then draw the bird, the pillar and the score
 	if not lostGame then
     	love.graphics.draw(flappyBird.img, flappyBird.x, flappyBird.y, math.rad(0), .2, .2)
     	drawPillar()
         love.graphics.print("Score: " .. tostring(score), 0, 0, 0, 2, 2)
     end
-
+    
+    -- If the game has ended, then display the score 
     if lostGame then
     	love.graphics.setBackgroundColor(200, 0, 0)
 		love.graphics.print("Splat! Score: " .. tostring(score), 175, 300, 0, 3, 3)
