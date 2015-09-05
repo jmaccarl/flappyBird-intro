9206fe1e598b465c6e12a2fb82b7e4672ff06b7e
Jenna <jenna.maccarley@gmail.com>
2015-09-05 01:06:51 -0400

    added random pillars and score

--- main.lua
+++ main.lua
@@ -11,11 +11,20 @@ function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
          y2 < y1+h1
 end
 
+function drawPillar()
+	love.graphics.rectangle('fill', pillar.x, 0, 50, pillar.y)
+	love.graphics.rectangle('fill', pillar.x, pillar.y + 200, 50, 800)
+end
+
+
 lostGame = false
 flappyBird = {x = 250, y = 300, speed = 150, img = nil}
+pillar = {x = 600, y = 400}
+score = 0
 
 function love.load(arg)
 	flappyBird.img = love.graphics.newImage('assets/flappy_bird.png')
+	love.graphics.setBackgroundColor(0, 0, 200)
 
 end
 
@@ -27,16 +36,27 @@ function love.update(dt)
 	end
 
 	if love.keyboard.isDown('up', 'w') then
-		flappyBird.speed = 150
+		flappyBird.speed = flappyBird.speed + 50
 	end
 
 	flappyBird.y = flappyBird.y - (flappyBird.speed*dt)
 
-	flappyBird.speed = flappyBird.speed - 10
+	flappyBird.speed = flappyBird.speed - 20
 
 	if flappyBird.y > 750 or flappyBird.y < 0 then
 		lostGame = true
 	end
+
+    if not lostGame then
+		pillar.x = pillar.x - 5;
+	end 
+
+	if pillar.x <= 0 then
+		pillar.x = 600
+		pillar.y = 200 + love.math.random(400)
+		score = score + 1
+	end
+
 
 end
 
@@ -44,10 +64,12 @@ end
 function love.draw(dt)
 	if not lostGame then
     	love.graphics.draw(flappyBird.img, flappyBird.x, flappyBird.y, math.rad(0), .2, .2)
+    	drawPillar()
+        love.graphics.print("Score: " .. tostring(score), 0, 0, 0, 2, 2)
     end
 
     if lostGame then
-		love.graphics.setColor(255, 0, 0, 255)
-		love.graphics.print("You lost!", 200, 300, 0, 3, 3)
+		love.graphics.print("Splat! Score: " .. tostring(score), 175, 300, 0, 3, 3)
 	end
+
 end
